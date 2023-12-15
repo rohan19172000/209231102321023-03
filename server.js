51a7f8270172412ab767432ec21f6bf64d11af34
rohan19172000 <152405220+rohan19172000@users.noreply.github.com>
2023-12-15 14:20:10 +0000

    express js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = 8081;
 
 const toDoList = ["hey everyone", "hope u all", "are doing", "awesome at ur work"]; // array
 
+
+
 // http://localhost:8081/todos
 app.get("/todos",(req, res)=>{
     // callback
@@ -17,6 +19,41 @@ app.get("/todos",(req, res)=>{
 })
 
 
+// http://localhost:8081/todos
+app.post("/todos", (req,res)=>{
+    let newDoItem = req.body.item;
+    toDoList.push(newDoItem);
+    res.status(201).send({
+        message: "The task was added sucessfully"
+    })
+})
+
+app.delete("/todos", (req,res)=>{
+var itemToDelete = req.body.item;
+
+toDoList.find((elem, index)=>{
+    if(elem === itemToDelete){
+        toDoList.splice(index, 1);
+    }
+});
+res.status(204).send({
+    message: `Deleted item ${req.body.item}`
+})
+})
+
+
+// put, patch
+app.all("/todos", (req, res)=>{
+    res.status(501).send({
+        message: "Not yet implemented"
+    })
+})
+
+app.all("*",(req,res)=>{
+    res.status(404).send({
+        message: "default url"
+    })
+})
 
 app.listen(port, ()=>{
     console.log(`NodeJs with Express started running sucesfuly on port ${port}`)
